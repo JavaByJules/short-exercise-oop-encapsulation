8dd4b6dc2e7c58d82bde38237fea7a94281f0648
JavaByJules <142684618+JavaByJules@users.noreply.github.com>
2023-08-26 04:06:08 +0100

    Encapsulation exercise
    Added pdf re description of methods re Dodgy and Secure Accounts. Also refactored WeatherReporter to replace Magic Numbers and or Strings with constants. Also giving functions meaningful names and paring down a lot of code meaning there's a lot less to maintain

--- src/main/java/com/techreturners/encapsulation/bankaccount/model/WeatherReporter.java
+++ src/main/java/com/techreturners/encapsulation/bankaccount/model/WeatherReporter.java
@@ -4,49 +4,46 @@ import java.text.MessageFormat;
 
 public class WeatherReporter {
 
-    public String location;
-    public double temperature;
+    private String location;
+    private double temperature;
 
+    private final String LONDON = "London";
+    private final String CALIFORNIA = "California";
+    private final String CAPETOWN = "Cape Town";
+
+    private final String TOOHOTPHRASE = "It's too hot 🥵!";
+    private final String TOOCOLDPHRASE = "It's too cold 🥶!";
+    private final String JUSTRIGHTPHRASE = "Ahhh...it's just right 😊!";
+    private final int hotTemperature = 30;
+    private final int coldTemperature = 10;
     public WeatherReporter(String location, double temperature) {
         this.location = location;
         this.temperature = temperature;
     }
 
-    public String print() {
-
-        double newTemp = (9.0 / 5.0) * temperature + 32;
-        return MessageFormat.format("I am in {0} and it is {1}. {2}. The temperature in Fahrenheit is {3}.", location, check1(), check2(), newTemp);
-
+    private double calculateTemperature(double temperature){
+        return (9.0 / 5.0) * temperature + 32;
+    }
+    public String printCityAndLocation() {
+        return MessageFormat.format("I am in {0} and it is {1}. {2}. The temperature in Fahrenheit is {3}.", location, checkLocation(), checkTemperature(), calculateTemperature(temperature));
     }
 
-    public String check1() {
-        if (location == "London") {
-
-            return "🌦";
-
-        } else if (location == "California") {
-
-            return "🌅";
-
-        } else if (location == "Cape Town") {
-
-            return "🌤";
-
-        }
-        return "🔆";
+    public String checkLocation() {
+        return switch (location) {
+            case LONDON -> "🌦";
+            case CALIFORNIA -> "🌅";
+            case CAPETOWN -> "🌤";
+            default -> "🔆";
+        };
     }
 
-    public String check2() {
-        if (temperature > 30) {
-
-            return "It's too hot 🥵!";
-
-        } else if (temperature < 10) {
-
-            return "It's too cold 🥶!";
-
+    public String checkTemperature() {
+        if (temperature > hotTemperature) {
+            return TOOHOTPHRASE;
+        } else if (temperature < coldTemperature) {
+            return TOOCOLDPHRASE;
         }
-        return "Ahhh...it's just right 😊!";
+        return JUSTRIGHTPHRASE;
     }
 
 }
